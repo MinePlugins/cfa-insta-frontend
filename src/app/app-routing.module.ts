4e2a7e29c4ee4d1932419166b6a88eaca1c3d8b4
MinePlugins <quentin.co@free.fr>
2021-03-04 17:28:57 +0100

    COMIIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { AdminContentComponent } from './admin-content/admin-content.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminGuard } from './services/admin.guard';
+import { AuthGuard } from './services/auth.guard';
+import { UserContentComponent } from './user-content/user-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: '',
+  component: HomeComponent,
+  canActivate: [AuthGuard],
+  canActivateChild: [AuthGuard],
+  children: [
+    {
+      path: 'user-content',
+      component: UserContentComponent
+    },
+    {
+      path: 'admin-content',
+      component: AdminContentComponent,
+      canActivate: [AdminGuard]
+    },
+    {
+      path: 'denied',
+      component: AccessDeniedComponent
+    }
+  ]
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'logout',
+  component: LoginComponent,
+  canActivate: [AuthGuard]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';  
+import {MatIconModule} from '@angular/material/icon';  
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { LoginComponent } from './login/login.component'; 
+import { AuthInterceptor } from './services/auth.interceptor';
+import { HomeComponent } from './home/home.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { AdminContentComponent } from './admin-content/admin-content.component';
+import { UserContentComponent } from './user-content/user-content.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AccessDeniedComponent,
+    AdminContentComponent,
+    UserContentComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
